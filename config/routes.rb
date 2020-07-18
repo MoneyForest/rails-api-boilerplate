d4ba04aa217704b44d96644da55855e988c4ad13
MoneyForest <kanamorishuhei@gmail.com>
2020-07-18 20:17:52 +0900

    add passwords and sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     scope :v1 do
       mount_devise_token_auth_for 'User', at: 'auth', controllers: {
-        registrations: 'api/v1/auth/registrations'
+        passwords: 'api/v1/auth/passwords',
+        registrations: 'api/v1/auth/registrations',
+        sessions: 'api/v1/auth/sessions'
       }
     end
   end
