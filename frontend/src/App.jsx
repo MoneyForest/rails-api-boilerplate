d95c43227345d0d03a93fa67b882d49835bb9a58
MoneyForest <kanamorishuhei@gmail.com>
2020-07-19 03:15:55 +0900

    add maincontainer component

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './assets/styles/style.css';
+import { MainContainer } from './components/index';
+
+const App = () => {
+  return (
+    <MainContainer />
+  );
+};
+
+export default App;
---
+++ frontend/src/components/MainContainer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const MainContainer = (props) => {
+  return (
+    <div className='main-container'>
+    </div>
+  );
+};
+
+export default MainContainer;
