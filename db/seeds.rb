941a6705d208e630e0a94e1db7d5401b0c5f9f7f
MoneyForest <kanamorishuhei@gmail.com>
2020-10-03 00:13:58 +0900

    feat: add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require './db/seeds/project'
+require './db/seeds/task'
+require './db/seeds/team'
+require './db/seeds/teams_project'
+require './db/seeds/user'
+require './db/seeds/users_team'
