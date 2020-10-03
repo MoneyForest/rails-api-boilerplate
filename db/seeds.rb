c5959ade8d312c50629d34dafda062dab9d0be51
MoneyForest <kanamorishuhei@gmail.com>
2020-10-03 18:21:44 +0900

    fix: seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,51 +1,46 @@
 company_name = Faker::Company.name
 
-10.times do |n|
-  Project.create!(
-    id: n,
-    name: "#{company_name} Project No.#{n + 1}",
-    description: "This is #{company_name} Project No.#{n + 1}",
-    team_id: 0,
-    archived: (n % 7).zero? ? 1 : 0,
-    created_at: Time.zone.now,
-    updated_at: Time.zone.now
-  )
-end
+Project.create!(
+  name: "#{company_name} Project No.1",
+  description: "This is #{company_name} Project No.1",
+  team_id: 0,
+  archived: 0,
+  created_at: Time.zone.now,
+  updated_at: Time.zone.now
+)
 
-10.times do |n|
+100.times do
   User.create!(
-    id: n,
-    name: "#{company_name} Project No.#{n + 1}",
-    description: "This is #{company_name} Project No.#{n + 1}",
-    team_id: 0,
-    archived: (n % 7).zero? ? 1 : 0,
+    email: Faker::Internet.email,
+    password: 'password',
+    password_confirmation: 'password',
     created_at: Time.zone.now,
     updated_at: Time.zone.now
   )
 end
 
-100.times do |n|
-  Task.create!(
-    id: n,
-    name: "#{company_name} Task No.#{n + 1}",
-    description: "This is #{company_name} Task No.#{n + 1}",
-    deadline: Time.zone.now,
-    repeats: nil,
-    completed: 0,
-    parent_id: nil,
-    assignee_id: nil,
-    creator_id: nil,
-    created_at: Time.zone.now,
-    updated_at: Time.zone.now
-  )
-end
-
-100.times do |n|
+10.times do |n|
   Team.create!(
-    id: n,
-    name: "#{company_name} Project No.#{n + 1}",
-    description: "This is #{company_name} Project No.#{n + 1}",
-    creator_id: nil,
+    name: "#{company_name} Team No.#{n + 1}",
+    creator_id: rand(1..100),
+    created_at: Time.zone.now,
+    updated_at: Time.zone.now
+  )
+end
+
+100.times do |n|
+  UsersTeam.create!(
+    user_id: n + 1,
+    team_id: n % 10 + 1,
+    created_at: Time.zone.now,
+    updated_at: Time.zone.now
+  )
+end
+
+10.times do |n|
+  TeamsProject.create!(
+    team_id: n + 1,
+    project_id: 1,
     created_at: Time.zone.now,
     updated_at: Time.zone.now
   )
