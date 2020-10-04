48715a8bc553328528b18108bf56f1edec06b842
MoneyForest <kanamorishuhei@gmail.com>
2020-10-04 14:49:28 +0900

    style: rubocop -a

--- app/controllers/api/v1/teams_controller.rb
+++ app/controllers/api/v1/teams_controller.rb
@@ -1,13 +1,12 @@
- class Api::V1::TeamsController < Api::V1::ApplicationController
-  before_action :set_team, only: %i(show update edit destroy)
+class Api::V1::TeamsController < Api::V1::ApplicationController
+  before_action :set_team, only: %i[show update edit destroy]
 
   def index
     users_team = current_user.users_team
     @teams = Team.where(id: users_team.pluck(:team_id))
   end
 
-  def show
-  end
+  def show; end
 
   def create
     @team = Team.create!(team_params.merge(creator_id: current_user.id))
@@ -25,7 +24,7 @@
 
   def team_params
     params.permit(
-        :name
+      :name
     )
   end
 
