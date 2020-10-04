3a98888e4fcaf636713a1d9387d8da9782e65a8a
MoneyForest <kanamorishuhei@gmail.com>
2020-10-04 15:39:09 +0900

    fix: seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,11 +3,31 @@ company_name = Faker::Company.name
 Project.create!(
   name: "#{company_name} Project No.1",
   description: "This is #{company_name} Project No.1",
-  team_id: 0,
   archived: 0,
   created_at: Time.zone.now,
   updated_at: Time.zone.now
 )
+
+100.times do |n|
+  Task.create!(
+      name: "#{company_name} Task No.#{n + 1}",
+      description: "This is #{company_name} Task No.1",
+      completed: 0,
+      parent_id: rand(1..100),
+      creator_id: rand(1..100),
+      created_at: Time.zone.now,
+      updated_at: Time.zone.now
+  )
+end
+
+10.times do |n|
+  Team.create!(
+      name: "#{company_name} Team No.#{n + 1}",
+      creator_id: rand(1..100),
+      created_at: Time.zone.now,
+      updated_at: Time.zone.now
+  )
+end
 
 100.times do
   User.create!(
@@ -19,21 +39,12 @@ Project.create!(
   )
 end
 
-10.times do |n|
-  Team.create!(
-    name: "#{company_name} Team No.#{n + 1}",
-    creator_id: rand(1..100),
-    created_at: Time.zone.now,
-    updated_at: Time.zone.now
-  )
-end
-
 100.times do |n|
-  UsersTeam.create!(
-    user_id: n + 1,
-    team_id: n % 10 + 1,
-    created_at: Time.zone.now,
-    updated_at: Time.zone.now
+  TasksProject.create!(
+      task_id: n + 1,
+      project_id: 1,
+      created_at: Time.zone.now,
+      updated_at: Time.zone.now
   )
 end
 
@@ -43,5 +54,32 @@ end
     project_id: 1,
     created_at: Time.zone.now,
     updated_at: Time.zone.now
+  )
+end
+
+100.times do |n|
+  UsersProject.create!(
+      user_id: n + 1,
+      project_id: 1,
+      created_at: Time.zone.now,
+      updated_at: Time.zone.now
+  )
+end
+
+100.times do |n|
+  UsersTask.create!(
+      user_id: n + 1,
+      task_id: n % 10 + 1,
+      created_at: Time.zone.now,
+      updated_at: Time.zone.now
+  )
+end
+
+100.times do |n|
+  UsersTeam.create!(
+      user_id: n + 1,
+      team_id: n % 10 + 1,
+      created_at: Time.zone.now,
+      updated_at: Time.zone.now
   )
 end
