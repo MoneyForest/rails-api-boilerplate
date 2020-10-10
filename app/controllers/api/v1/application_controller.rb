6a012986d370efdc923916c4d10c358d8b1dc025
MoneyForest <kanamorishuhei@gmail.com>
2020-10-11 01:14:11 +0900

    refactor: fix by reek

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -1,13 +1,13 @@
 class Api::V1::ApplicationController < ApplicationController
-  rescue_from Exception do |e|
-    render json: { errors: [e.message] }, status: :internal_server_error
+  rescue_from Exception do |error|
+    render json: { errors: [error.message] }, status: :internal_server_error
   end
 
-  rescue_from ActiveRecord::RecordInvalid do |e|
-    render json: { errors: [e.message] }, status: :unprocessable_entity
+  rescue_from ActiveRecord::RecordInvalid do |error|
+    render json: { errors: [error.message] }, status: :unprocessable_entity
   end
 
-  rescue_from ActiveRecord::RecordNotFound do |e|
-    render json: { errors: [e.message] }, status: :not_found
+  rescue_from ActiveRecord::RecordNotFound do |error|
+    render json: { errors: [error.message] }, status: :not_found
   end
 end
