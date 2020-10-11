6606f44946f391c6f118880018e4cd2472a5ceee
MoneyForest <kanamorishuhei@gmail.com>
2020-10-12 01:46:45 +0900

    test: POST /api/v1/users

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -1,6 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe '/api/v1/users', type: :request do
+  describe 'POST /api/v1/users' do
+    include_context 'with committer schema check'
+
+    let(:headers) do
+      {
+          'Accept' => 'application/json',
+          'Content-Type' => 'application/json'
+      }
+    end
+
+    context 'when valid request' do
+      include_examples 'committer schema check'
+
+      let(:params) do
+        {
+            'email' => Faker::Internet.email,
+            'password' => 'password'
+        }
+      end
+
+      it 'returns a 200 response' do
+        subject
+        expect(response).to have_http_status :ok
+      end
+    end
+  end
+
   describe 'POST /api/v1/users/authenticate' do
     include_context 'with committer schema check'
     include_context 'with a defined user'
