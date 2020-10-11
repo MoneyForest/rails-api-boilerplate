be4ff64ab51c8a070755751619131528e1f3d9d2
MoneyForest <kanamorishuhei@gmail.com>
2020-10-12 01:46:28 +0900

    test: fix simplecov derectory

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,7 +7,7 @@ SimpleCov.start 'rails' do
   add_filter 'app/channels'
   add_filter 'app/jobs'
   add_filter 'app/mailers'
-  add_filter 'lib'
+  add_filter 'lib/tasks'
 end
 
 ENV['RAILS_ENV'] ||= 'test'
