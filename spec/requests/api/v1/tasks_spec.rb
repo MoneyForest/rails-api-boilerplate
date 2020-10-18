f8730d9d6540592dc1640885a612fd89d0fdec7c
MoneyForest <kanamorishuhei@gmail.com>
2020-10-18 20:41:30 +0900

    fix: rspecs

--- spec/requests/api/v1/tasks_spec.rb
+++ spec/requests/api/v1/tasks_spec.rb
@@ -40,4 +40,198 @@ RSpec.describe '/api/v1/tasks', type: :request do
       end
     end
   end
+
+  describe 'POST /api/v1/tasks' do
+    include_context 'with committer schema check'
+    include_context 'with a defined user'
+
+    context 'when valid request' do
+      include_context 'with valid authenticated request headers'
+
+      context 'with a task' do
+        include_examples 'committer schema check'
+
+        let(:params) { { name: "task", description: "This is task" } }
+
+        it 'returns a 200 response' do
+          subject
+          expect(request.path).to eq api_v1_tasks_path
+          expect(response).to have_http_status :ok
+          expect(Task.find(JSON.parse(response.body)['task']['id'])).to_not be_nil
+        end
+      end
+    end
+
+    context 'when invalid request' do
+      context 'when header is invalid' do
+        include_context 'with invalid authenticated request headers'
+        include_examples 'committer schema check'
+
+        let(:params) { { name: "task", description: "This is task" } }
+
+        it 'returns a 401 response' do
+          subject
+          expect(request.path).to eq api_v1_tasks_path
+          expect(response).to have_http_status :unauthorized
+        end
+      end
+    end
+  end
+
+  describe 'GET /api/v1/tasks/:id' do
+    include_context 'with committer schema check'
+    include_context 'with a task associated user'
+    include_context 'with a defined user'
+
+    context 'when valid request' do
+      include_context 'with valid authenticated request headers'
+
+      context 'with a task' do
+        include_context 'with a task associated user'
+        include_examples 'committer schema check'
+
+        let(:id) { task.id }
+
+        it 'returns a 200 response' do
+          subject
+          expect(request.path).to eq api_v1_task_path(task)
+          expect(response).to have_http_status :ok
+        end
+      end
+    end
+
+    context 'when invalid request' do
+      context 'when header is invalid' do
+        include_context 'with invalid authenticated request headers'
+        include_examples 'committer schema check'
+
+        let(:id) { task.id }
+
+        it 'returns a 401 response' do
+          subject
+          expect(request.path).to eq api_v1_task_path(task)
+          expect(response).to have_http_status :unauthorized
+        end
+      end
+
+      context 'with no task' do
+        include_context 'with a task associated user'
+        include_context 'with valid authenticated request headers'
+        include_examples 'committer schema check'
+
+        let(:id) { task.id + 1}
+
+        it 'returns a 404 response' do
+          subject
+          expect(request.path).to eq api_v1_task_path(task.id + 1)
+          expect(response).to have_http_status :not_found
+        end
+      end
+    end
+  end
+
+  describe 'PUT /api/v1/tasks/:id' do
+    include_context 'with committer schema check'
+    include_context 'with a task associated user'
+    include_context 'with a defined user'
+
+    context 'when valid request' do
+      include_context 'with valid authenticated request headers'
+
+      context 'with a task' do
+        include_examples 'committer schema check'
+
+        let(:id) { task.id }
+        let(:params) { { name: "task", description: "This is task", is_archived: true } }
+
+        it 'returns a 200 response' do
+          subject
+          expect(request.path).to eq api_v1_task_path(task)
+          expect(response).to have_http_status :ok
+        end
+      end
+    end
+
+    context 'when invalid request' do
+      context 'when header is invalid' do
+        include_context 'with invalid authenticated request headers'
+        include_examples 'committer schema check'
+
+        let(:id) { task.id }
+        let(:params) { { name: "task", description: "This is task" } }
+
+        it 'returns a 401 response' do
+          subject
+          expect(request.path).to eq api_v1_task_path(task)
+          expect(response).to have_http_status :unauthorized
+        end
+      end
+
+      context 'with no task' do
+        include_context 'with a task associated user'
+        include_context 'with valid authenticated request headers'
+        include_examples 'committer schema check'
+
+        let(:id) { task.id + 1}
+
+        it 'returns a 404 response' do
+          subject
+          expect(request.path).to eq api_v1_task_path(task.id + 1)
+          expect(response).to have_http_status :not_found
+        end
+      end
+    end
+  end
+
+  describe 'DELETE /api/v1/tasks/:id' do
+    include_context 'with committer schema check'
+    include_context 'with a task associated user'
+    include_context 'with a defined user'
+
+    context 'when valid request' do
+      include_context 'with valid authenticated request headers'
+
+      context 'with a task' do
+        include_examples 'committer schema check'
+
+        let(:id) { task.id }
+
+        it 'returns a 200 response' do
+          subject
+          expect(request.path).to eq api_v1_task_path(task)
+          expect(response).to have_http_status :ok
+        end
+      end
+    end
+
+    context 'when invalid request' do
+      context 'when header is invalid' do
+        include_context 'with invalid authenticated request headers'
+        include_examples 'committer schema check'
+
+        let(:id) { task.id }
+        let(:params) { { name: "task", description: "This is task" } }
+
+        it 'returns a 401 response' do
+          subject
+          expect(request.path).to eq api_v1_task_path(task)
+          expect(response).to have_http_status :unauthorized
+        end
+      end
+
+      context 'with no task' do
+        include_context 'with a task associated user'
+        include_context 'with valid authenticated request headers'
+        include_examples 'committer schema check'
+
+        let(:id) { task.id + 1}
+
+        it 'returns a 404 response' do
+          subject
+          expect(request.path).to eq api_v1_task_path(task.id + 1)
+          expect(response).to have_http_status :not_found
+        end
+      end
+    end
+  end
 end
--- spec/requests/api/v1/teams_spec.rb
+++ spec/requests/api/v1/teams_spec.rb
@@ -40,4 +40,198 @@ RSpec.describe '/api/v1/teams', type: :request do
       end
     end
   end
+
+  describe 'POST /api/v1/teams' do
+    include_context 'with committer schema check'
+    include_context 'with a defined user'
+
+    context 'when valid request' do
+      include_context 'with valid authenticated request headers'
+
+      context 'with a team' do
+        include_examples 'committer schema check'
+
+        let(:params) { { name: "team", description: "This is team" } }
+
+        it 'returns a 200 response' do
+          subject
+          expect(request.path).to eq api_v1_teams_path
+          expect(response).to have_http_status :ok
+          expect(Team.find(JSON.parse(response.body)['team']['id'])).to_not be_nil
+        end
+      end
+    end
+
+    context 'when invalid request' do
+      context 'when header is invalid' do
+        include_context 'with invalid authenticated request headers'
+        include_examples 'committer schema check'
+
+        let(:params) { { name: "team", description: "This is team" } }
+
+        it 'returns a 401 response' do
+          subject
+          expect(request.path).to eq api_v1_teams_path
+          expect(response).to have_http_status :unauthorized
+        end
+      end
+    end
+  end
+
+  describe 'GET /api/v1/teams/:id' do
+    include_context 'with committer schema check'
+    include_context 'with a team associated user'
+    include_context 'with a defined user'
+
+    context 'when valid request' do
+      include_context 'with valid authenticated request headers'
+
+      context 'with a team' do
+        include_context 'with a team associated user'
+        include_examples 'committer schema check'
+
+        let(:id) { team.id }
+
+        it 'returns a 200 response' do
+          subject
+          expect(request.path).to eq api_v1_team_path(team)
+          expect(response).to have_http_status :ok
+        end
+      end
+    end
+
+    context 'when invalid request' do
+      context 'when header is invalid' do
+        include_context 'with invalid authenticated request headers'
+        include_examples 'committer schema check'
+
+        let(:id) { team.id }
+
+        it 'returns a 401 response' do
+          subject
+          expect(request.path).to eq api_v1_team_path(team)
+          expect(response).to have_http_status :unauthorized
+        end
+      end
+
+      context 'with no team' do
+        include_context 'with a team associated user'
+        include_context 'with valid authenticated request headers'
+        include_examples 'committer schema check'
+
+        let(:id) { team.id + 1}
+
+        it 'returns a 404 response' do
+          subject
+          expect(request.path).to eq api_v1_team_path(team.id + 1)
+          expect(response).to have_http_status :not_found
+        end
+      end
+    end
+  end
+
+  describe 'PUT /api/v1/teams/:id' do
+    include_context 'with committer schema check'
+    include_context 'with a team associated user'
+    include_context 'with a defined user'
+
+    context 'when valid request' do
+      include_context 'with valid authenticated request headers'
+
+      context 'with a team' do
+        include_examples 'committer schema check'
+
+        let(:id) { team.id }
+        let(:params) { { name: "team", description: "This is team", is_archived: true } }
+
+        it 'returns a 200 response' do
+          subject
+          expect(request.path).to eq api_v1_team_path(team)
+          expect(response).to have_http_status :ok
+        end
+      end
+    end
+
+    context 'when invalid request' do
+      context 'when header is invalid' do
+        include_context 'with invalid authenticated request headers'
+        include_examples 'committer schema check'
+
+        let(:id) { team.id }
+        let(:params) { { name: "team", description: "This is team" } }
+
+        it 'returns a 401 response' do
+          subject
+          expect(request.path).to eq api_v1_team_path(team)
+          expect(response).to have_http_status :unauthorized
+        end
+      end
+
+      context 'with no team' do
+        include_context 'with a team associated user'
+        include_context 'with valid authenticated request headers'
+        include_examples 'committer schema check'
+
+        let(:id) { team.id + 1}
+
+        it 'returns a 404 response' do
+          subject
+          expect(request.path).to eq api_v1_team_path(team.id + 1)
+          expect(response).to have_http_status :not_found
+        end
+      end
+    end
+  end
+
+  describe 'DELETE /api/v1/teams/:id' do
+    include_context 'with committer schema check'
+    include_context 'with a team associated user'
+    include_context 'with a defined user'
+
+    context 'when valid request' do
+      include_context 'with valid authenticated request headers'
+
+      context 'with a team' do
+        include_examples 'committer schema check'
+
+        let(:id) { team.id }
+
+        it 'returns a 200 response' do
+          subject
+          expect(request.path).to eq api_v1_team_path(team)
+          expect(response).to have_http_status :ok
+        end
+      end
+    end
+
+    context 'when invalid request' do
+      context 'when header is invalid' do
+        include_context 'with invalid authenticated request headers'
+        include_examples 'committer schema check'
+
+        let(:id) { team.id }
+        let(:params) { { name: "team", description: "This is team" } }
+
+        it 'returns a 401 response' do
+          subject
+          expect(request.path).to eq api_v1_team_path(team)
+          expect(response).to have_http_status :unauthorized
+        end
+      end
+
+      context 'with no team' do
+        include_context 'with a team associated user'
+        include_context 'with valid authenticated request headers'
+        include_examples 'committer schema check'
+
+        let(:id) { team.id + 1}
+
+        it 'returns a 404 response' do
+          subject
+          expect(request.path).to eq api_v1_team_path(team.id + 1)
+          expect(response).to have_http_status :not_found
+        end
+      end
+    end
+  end
 end
